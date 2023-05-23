fd496b43bbca6525715da77c72711485905ca8c8
MikaëlLESGUER <mikedev35@outlook.fr>
2023-05-23 12:21:46 +0000

    mise en place du mini projet

--- exercices-fonctions/index.js
+++ exercices-fonctions/index.js
@@ -12,9 +12,9 @@ console.log(mult(21, 2)); // doit afficher 42
 console.log(div(84, 2)); // doit afficher 42
 console.log(mod(42, 2)); // doit afficher 0
 
-function add (value) {
+function add (a, b) {
     
-    return  value + value;
+    return  a + b;
 
 }
 
@@ -61,37 +61,60 @@ mod();
 
 function askUserOperation() {
     var operation = prompt("Entrez l'opération à effectuer (+, -, *, /) :");
-    console.log(operation)
+    return operation;
 }
 
 function askUserFirstNumber() {
     var nb1 = parseInt(window.prompt("Choisissez un nombre"));
-    console.log(nb1)
+    return nb1;
 }
 
 function askUserSecondNumber() {
     var nb2 = parseInt(window.prompt("Choisissez un nombre"));
+    return nb2;
+    // console.log(nb2);
 }
 
 // askUserOperation();
 // askUserFirstNumber();
 // askUserSecondNumber();
 
+
 // Exercice 3 : une fonction pour les gouverner toutes
 // Créer une fonction calculator qui va appeler les fonctions de l'exercice 2 pour récupérer les infos de l'utilisateur puis va appeler les fonctions de l'exercice 1 selon ce qu'a choisi l'utilisateur.
 
 //     N'oubliez pas d'appeler votre fonction calculator Ã  la fin de votre code pour que cela puisse fonctionner
+calculator();
 
 function calculator () {
-    askUserOperation();
-    askUserFirstNumber();
-    askUserSecondNumber();
     
-    add();
-    sub();
-    mult();
-    div();
-    mod();
+    let operator = askUserOperation();
+    let firstNumber = askUserFirstNumber();
+    let secondNumber = askUserSecondNumber();
+    
+        if(operator === "+")
+    {
+        add(firstNumber, secondNumber);
+        console.log(add(firstNumber, secondNumber));
+    }
+    else if (operator === "-")
+    {
+        sub(firstNumber,secondNumber);
+        console.log(sub(firstNumber, secondNumber));
+    }
+    else if(operator === "*")
+    {
+        mult(firstNumber,secondNumber);
+        console.log(mult(firstNumber, secondNumber));
+    }
+    else if(operator === "/")
+    {
+        div(firstNumber,secondNumber);
+        console.log(div(firstNumber, secondNumber));
+    }
+    else if(operator === "%")
+    {
+        mod(firstNumber,secondNumber);
+        console.log(mod(firstNumber, secondNumber));
+    }
 }
-
-calculator()
