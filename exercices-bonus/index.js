070b413a3e8c998151bf9de2a54fa6bbaf0d834c
MikaëlLESGUER <mikedev35@outlook.fr>
2023-05-23 15:14:48 +0000

    mise en place excercice bonus V.2

--- exercices-bonus/index.js
+++ exercices-bonus/index.js
@@ -1,6 +1,17 @@
 // Exercice 1 : trier des nombres
 // Créer une fonction sortNumbers qui retourne un tableau avec les nombres du tableau numbers triés du plus petit au plus grand.
 
-let numbers = [-23,42,21,90,54,362,-12,57,78,84,35,546,128,7,15,65,28];
+// let numbers = [-23,42,21,90,54,362,-12,57,78,84,35,546,128,7,15,65,28];
 
-function sortNumbers =;
+// function sortNumbers() {
+    
+// } ;
+
+function sortNumbers(numbers) {
+  return numbers((a, b) => a - b);
+}
+
+let numbers = [-23, 42, 21, 90, 54, 362, -12, 57, 78, 84, 35, 546, 128, 7, 15, 65, 28];
+let sortedNumbers = sortNumbers(numbers);
+
+console.log(sortedNumbers);
